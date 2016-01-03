d78b1cea73e8188b74614dc4da7e200bf9c72e17
Paramvir Dehal <psdehal@lbl.gov>
2016-01-03 12:15:57 -0800

    Added trimmomatic code and one test

--- lib/kb_trimmomatic/kb_trimmomaticImpl.py
+++ lib/kb_trimmomatic/kb_trimmomaticImpl.py
@@ -2,6 +2,10 @@
 import sys
 import traceback
 from biokbase.workspace.client import Workspace as workspaceService
+import requests
+import subprocess
+import os
+import re
 #END_HEADER
 
 
@@ -23,6 +27,79 @@ This sample module contains one small method - filter_contigs.
     #########################################
     #BEGIN_CLASS_HEADER
     workspaceURL = None
+    TRIMMOMATIC = 'java -jar /kb/module/Trimmomatic-0.33/trimmomatic-0.33.jar'
+    ADAPTER_DIR = '/kb/module/Trimmomatic-0.33/adapters/'
+
+
+    def parse_trimmomatic_steps(self, input_params):
+        # validate input parameters and return string defining trimmomatic steps
+
+        parameter_string = ''
+
+        if 'read_type' not in input_params and input_params['read_type'] is not None:
+            raise ValueError('read_type not defined')
+        elif input_params['read_type'] not in ('PE', 'SE'):
+            raise ValueError('read_type must be PE or SE')
+
+        if 'quality_encoding' not in input_params and input_params['quality_encoding'] is not None:
+            raise ValueError('quality_encoding not defined')
+        elif input_params['quality_encoding'] not in ('phred33', 'phred64'):
+            raise ValueError('quality_encoding must be phred33 or phred64')
+
+        # set adapter trimming
+        if ('adapterFa' in input_params and input_params['adapterFa'] is not None and
+            'seed_mismatches' in input_params and input_params['seed_mismatches'] is not None and
+            'palindrome_clip_threshold' in input_params and input_params['quality_encoding'] is not None and
+            'simple_clip_threshold' in input_params and input_params['simple_clip_threshold'] is not None):
+            parameter_string = ("ILLUMINACLIP:" + self.ADAPTER_DIR +
+                                    ":".join( (input_params['adapterFa'],
+                                       input_params['seed_mismatches'], 
+                                       input_params['palindrome_clip_threshold'],
+                                       input_params['simple_clip_threshold']) ) + " " )
+        elif ( ('adapterFa' in input_params and input_params['adapterFa'] is not None) or
+               ('seed_mismatches' in input_params and input_params['seed_mismatches'] is not None) or
+               ('palindrome_clip_threshold' in input_params and input_params['palindrome_clip_threshold'] is not None) or
+               ('simple_clip_threshold' in input_params and input_params['simple_clip_threshold'] is not None) ):
+            raise ValueError('Adapter Cliping requires Adapter, Seed Mismatches, Palindrome Clip Threshold and Simple Clip Threshold')
+
+
+        # set Crop
+        if 'crop_length' in input_params and input_params['crop_length'] is not None:
+            parameter_string += 'CROP:' + input_params['crop_length'] + ' '
+
+        # set Headcrop
+        if 'head_crop_length' in input_params and input_params['head_crop_length'] is not None:
+            parameter_string += 'HEADCROP:' + input_params['head_crop_length'] + ' '
+
+
+        # set Leading
+        if 'leading_min_quality' in input_params and input_params['leading_min_quality'] is not None:
+            parameter_string += 'LEADING:' + input_params['leading_min_quality'] + ' '
+
+
+        # set Trailing
+        if 'trailing_min_quality' in input_params and input_params['trailing_min_quality'] is not None:
+            parameter_string += 'TRAILING:' + input_params['trailing_min_quality'] + ' '
+
+
+        # set sliding window
+        if ('sliding_window_size' in input_params and input_params['sliding_window_size'] is not None and 
+            'sliding_window_min_quality' in input_params and input_params['sliding_window_min_quality'] is not None):
+            parameter_string += 'SLIDINGWINDOW:' + input_params['sliding_window_size'] + ":" + input_params['sliding_window_min_quality'] + ' '
+        elif ( ('sliding_window_size' in input_params and input_params['sliding_window_size'] is not None) or 
+               ('sliding_window_min_quality' in input_params and input_params['sliding_window_min_quality'] is not None) ):
+            raise ValueError('Sliding Window filtering requires both Window Size and Window Minimum Quality to be set')
+            
+
+        # set min length
+        if 'min_length' in input_params and input_params['min_length'] is not None:
+            parameter_string += 'MINLEN:' + input_params['min_length'] + ' '
+
+        if parameter_string == '':
+            raise ValueError('No filtering/trimming steps specified!')
+
+        return parameter_string
+
     #END_CLASS_HEADER
 
     # config contains contents of config file in a hash or None if it couldn't
@@ -30,6 +107,7 @@ This sample module contains one small method - filter_contigs.
     def __init__(self, config):
         #BEGIN_CONSTRUCTOR
         self.workspaceURL = config['workspace-url']
+        self.shockURL = config['shock-url']
         #END_CONSTRUCTOR
         pass
 
@@ -131,6 +209,150 @@ This sample module contains one small method - filter_contigs.
         # ctx is the context object
         # return variables are: report
         #BEGIN runTrimmomatic
+        token = ctx['token']
+        wsClient = workspaceService(self.workspaceURL, token=token)
+        headers = {'Authorization': 'OAuth '+token}
+
+        if ('output_ws' not in input_params or input_params['output_ws'] is None):
+            input_params['output_ws'] = input_params['input_ws']
+
+        trimmomatic_params  = self.parse_trimmomatic_steps(input_params)
+        trimmomatic_options = input_params['read_type'] + ' -' + input_params['quality_encoding']
+
+        report = ''
+
+
+        try:
+            readLibrary = wsClient.get_objects([{'name': input_params['input_read_library'], 
+                                                            'workspace' : input_params['input_ws']}])[0]
+        except Exception as e:
+            raise ValueError('Unable to get read library object from workspace: (' + input_params['input_ws']+ '/' + input_params['input_read_library'] +')' + str(e))
+
+
+        if input_params['read_type'] == 'PE':
+            if 'lib1' in readLibrary['data']:
+                forward_reads = readLibrary['data']['lib1']['file']
+            elif 'handle_1' in readLibrary['data']:
+                forward_reads = readLibrary['data']['handle_1']
+            if 'lib2' in readLibrary['data']:
+                reverse_reads = readLibrary['data']['lib2']['file']
+            elif 'handle_2' in readLibrary['data']:
+                reverse_reads = readLibrary['data']['handle_2']
+            else:
+                reverse_reads={}
+
+            forward_reads_file = open(forward_reads['file_name'], 'w', 0)
+            r = requests.get(forward_reads['url']+'/node/'+forward_reads['id']+'?download', stream=True, headers=headers)
+            for chunk in r.iter_content(1024):
+                forward_reads_file.write(chunk)
+
+            if 'interleaved' in readLibrary['data'] and readLibrary['data']['interleaved']:
+                if re.search('gz', forward_reads['file_name'], re.I):
+                    bcmdstring = 'gunzip -c ' + forward_reads['file_name']
+                else:    
+                    bcmdstring = 'cat ' + forward_reads['file_name'] 
+
+                
+                cmdstring = bcmdstring + '| (paste - - - - - - - -  | tee >(cut -f 1-4 | tr "\t" "\n" > forward.fastq) | cut -f 5-8 | tr "\t" "\n" > reverse.fastq )'
+                cmdProcess = subprocess.Popen(cmdstring, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, executable='/bin/bash')
+                stdout, stderr = cmdProcess.communicate()
+
+                # Check return status
+                report = "cmdstring: " + cmdstring + " stdout: " + stdout + " stderr: " + stderr
+                
+                forward_reads['file_name']='forward.fastq'
+                reverse_reads['file_name']='reverse.fastq'
+            else:
+                reverse_reads_file = open(reverse_reads['file_name'], 'w', 0)
+                r = requests.get(reverse_reads['url']+'/node/'+reverse_reads['id']+'?download', stream=True, headers=headers)
+                for chunk in r.iter_content(1024):
+                    reverse_reads_file.write(chunk)
+
+            cmdstring = " ".join( (self.TRIMMOMATIC, trimmomatic_options, 
+                            forward_reads['file_name'], 
+                            reverse_reads['file_name'],
+                            'forward_paired_'   +forward_reads['file_name'],
+                            'forward_unpaired_' +forward_reads['file_name'],
+                            'reverse_paired_'   +reverse_reads['file_name'],
+                            'reverse_unpaired_' +reverse_reads['file_name'],
+                            trimmomatic_params) )
+
+            cmdProcess = subprocess.Popen(cmdstring, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+
+            stdout, stderr = cmdProcess.communicate()
+            report += "cmdstring: " + cmdstring + " stdout: " + stdout + " stderr " + stderr
+
+
+            #get read counts
+            match = re.search(r'Both Surviving: (\d+).*?Forward Only Surviving: (\d+).*?Reverse Only Surviving: (\d+)', report)
+            read_count_paired = match.group(1)
+            read_count_forward_only = match.group(2)
+            read_count_reverse_only = match.group(3)
+
+            #upload reads forward paired
+            cmdstring = " ".join( ('ws-tools fastX2reads --inputfile', 'forward_paired_' + forward_reads['file_name'], 
+                                   '--wsurl', self.workspaceURL, '--shockurl', self.shockURL, '--outws', input_params['output_ws'],
+                                   '--outobj', input_params['output_read_library'] + '_forward_paired', '--readcount', read_count_paired, '--token', token ) )
+
+            cmdProcess = subprocess.Popen(cmdstring, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            stdout, stderr = cmdProcess.communicate()
+            report += "cmdstring: " + cmdstring + " stdout: " + stdout + " stderr: " + stderr
+
+            #upload reads reverse paired
+            cmdstring = " ".join( ('ws-tools fastX2reads --inputfile', 'reverse_paired_' + reverse_reads['file_name'], 
+                                   '--wsurl', self.workspaceURL, '--shockurl', self.shockURL, '--outws', input_params['output_ws'],
+                                   '--outobj', input_params['output_read_library'] + '_reverse_paired', '--readcount', read_count_paired, '--token', token ) )
+
+            cmdProcess = subprocess.Popen(cmdstring, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            stdout, stderr = cmdProcess.communicate()
+            report += "cmdstring: " + cmdstring + " stdout: " + stdout + " stderr: " + stderr
+
+            #upload reads forward unpaired
+            cmdstring = " ".join( ('ws-tools fastX2reads --inputfile', 'forward_unpaired_' + forward_reads['file_name'], 
+                                   '--wsurl', self.workspaceURL, '--shockurl', self.shockURL, '--outws', input_params['output_ws'],
+                                   '--outobj', input_params['output_read_library'] + '_forward_unpaired', '--readcount', read_count_forward_only, '--token', token ) )
+
+            cmdProcess = subprocess.Popen(cmdstring, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            stdout, stderr = cmdProcess.communicate()
+            report += "cmdstring: " + cmdstring + " stdout: " + stdout + " stderr: " + stderr
+
+            #upload reads reverse unpaired
+            cmdstring = " ".join( ('ws-tools fastX2reads --inputfile', 'reverse_unpaired_' + reverse_reads['file_name'], 
+                                   '--wsurl', self.workspaceURL, '--shockurl', self.shockURL, '--outws', input_params['output_ws'],
+                                   '--outobj', input_params['output_read_library'] + '_reverse_unpaired', '--readcount', read_count_reverse_only, '--token', token ) )
+
+            cmdProcess = subprocess.Popen(cmdstring, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            stdout, stderr = cmdProcess.communicate()
+            report += "cmdstring: " + cmdstring + " stdout: " + stdout + " stderr: " + stderr
+
+        else:
+            if 'handle' in readLibrary['data']:
+                reads_file = open(readLibrary['data']['handle']['file_name'], 'w', 0)
+                r = requests.get(readLibrary['data']['handle']['url']+'/node/'+readLibrary['data']['handle']['id']+'?download', stream=True, headers=headers)
+                for chunk in r.iter_content(1024):
+                    reads_file.write(chunk)
+
+            cmdstring = " ".join( (self.TRIMMOMATIC, trimmomatic_options,
+                            readLibrary['data']['handle']['file_name'],
+                            'trimmed_' + readLibrary['data']['handle']['file_name'],
+                            trimmomatic_params) )
+
+            cmdProcess = subprocess.Popen(cmdstring, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            stdout, stderr = cmdProcess.communicate()
+            report = "cmdstring: " + cmdstring + " stdout: " + stdout + " stderr: " + stderr
+
+            #get read count
+            match = re.search(r'Surviving: (\d+)', report)
+            readcount = match.group(1)
+
+            #upload reads
+            cmdstring = " ".join( ('ws-tools fastX2reads --inputfile', 'trimmed_' + readLibrary['data']['handle']['file_name'], 
+                                   '--wsurl', self.workspaceURL, '--shockurl', self.shockURL, '--outws', input_params['output_ws'],
+                                   '--outobj', input_params['output_read_library'], '--readcount', readcount, '--token', token ) )
+
+            cmdProcess = subprocess.Popen(cmdstring, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            stdout, stderr = cmdProcess.communicate()
+            report += "cmdstring: " + cmdstring + " stdout: " + stdout + " stderr: " + stderr
         #END runTrimmomatic
 
         # At some point might do deeper type checking...
--- test/kb_trimmomatic_server_test.py
+++ test/kb_trimmomatic_server_test.py
@@ -89,4 +89,29 @@ class kb_trimmomaticTest(unittest.TestCase):
             self.getImpl().filter_contigs(self.getContext(), {'workspace': self.getWsName(), 
                 'contigset_id': 'fake', 'min_length': 'ten'})
         self.assertTrue('Cannot parse integer from min_length parameter' in str(context.exception))
+
+    def test_trimmomatic_ok(self):
+        print "run trim"
+        #ctx={'token':self.token}
+        input_params={}
+        input_params['input_ws']='psdehal:1446073144048'
+        input_params['input_read_library']='rhodo.art.q20.int.PE.reads'
+        input_params['read_type']='PE'
+        input_params['quality_encoding']='phred33'
+        input_params['adapterFa']='TruSeq3-PE.fa'
+        input_params['seed_mismatches']='2'
+        input_params['palindrome_clip_threshold']='30'
+        input_params['simple_clip_threshold']='10'
+        input_params['crop_length']='200'
+        input_params['head_crop_length']='1'
+        input_params['leading_min_quality']='1'
+        input_params['trailing_min_quality']='1'
+        input_params['sliding_window_size']='4'
+        input_params['sliding_window_min_quality']='15'
+        input_params['min_length']='100'
+        input_params['output_read_library']='testing'
+        input_params['output_ws'] = input_params['input_ws']
+        result=self.getImpl().runTrimmomatic(self.getContext(), input_params)
+        print result
+
         
