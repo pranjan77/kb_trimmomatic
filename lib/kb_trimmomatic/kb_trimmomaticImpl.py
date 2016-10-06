9001451d0d1c818d76f91defb3811523324a8c88
Dylan Chivian <DCChivian@lbl.gov>
2016-10-06 15:43:17 -0700

    update Trimmomatic from v0.33 to v0.36

--- lib/kb_trimmomatic/kb_trimmomaticImpl.py
+++ lib/kb_trimmomatic/kb_trimmomaticImpl.py
@@ -44,8 +44,8 @@ execTrimmomaticSingleLibrary() runs Trimmomatic on a single library
     
     #BEGIN_CLASS_HEADER
     workspaceURL = None
-    TRIMMOMATIC = 'java -jar /kb/module/Trimmomatic-0.33/trimmomatic-0.33.jar'
-    ADAPTER_DIR = '/kb/module/Trimmomatic-0.33/adapters/'
+    TRIMMOMATIC = 'java -jar /kb/module/Trimmomatic-0.36/trimmomatic-0.36.jar'
+    ADAPTER_DIR = '/kb/module/Trimmomatic-0.36/adapters/'
 
     def log(self, target, message):
         if target is not None:
