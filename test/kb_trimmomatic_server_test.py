223a4abbbf3bfa61193c4987ed192e6feecf5a17
Dylan Chivian <dcchivian@lbl.gov>
2016-12-19 18:12:36 -0800

    updated tests

--- test/kb_trimmomatic_server_test.py
+++ test/kb_trimmomatic_server_test.py
@@ -1,14 +1,24 @@
+# -*- coding: utf-8 -*-
 import unittest
 import os
 import json
 import time
+import requests
 
 from os import environ
-from ConfigParser import ConfigParser
+try:
+    from ConfigParser import ConfigParser  # py2
+except:
+    from configparser import ConfigParser  # py3
+
 from pprint import pprint
 
+from requests_toolbelt import MultipartEncoder
 from biokbase.workspace.client import Workspace as workspaceService
+from biokbase.AbstractHandle.Client import AbstractHandle as HandleService
+
 from kb_trimmomatic.kb_trimmomaticImpl import kb_trimmomatic
+from kb_trimmomatic.kb_trimmomaticServer import MethodContext
 
 
 class kb_trimmomaticTest(unittest.TestCase):
@@ -16,9 +26,22 @@ class kb_trimmomaticTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         token = environ.get('KB_AUTH_TOKEN', None)
-        cls.ctx = {'token': token, 'provenance': [{'service': 'kb_trimmomatic',
-            'method': 'please_never_use_it_in_production', 'method_params': []}],
-            'authenticated': 1}
+        cls.ctx = {'token': token}
+        cls.token = token
+        user_id = requests.post(
+            'https://kbase.us/services/authorization/Sessions/Login',
+            data='token={}&fields=user_id'.format(token)).json()['user_id']
+        # WARNING: don't call any logging methods on the context object,
+        # it'll result in a NoneType error
+        cls.ctx = MethodContext(None)
+        cls.ctx.update({'token': token,
+                        'user_id': user_id,
+                        'provenance': [
+                            {'service': 'kb_trimmomatic',
+                             'method': 'please_never_use_it_in_production',
+                             'method_params': []
+                             }],
+                        'authenticated': 1})
         config_file = environ.get('KB_DEPLOYMENT_CONFIG', None)
         cls.cfg = {}
         config = ConfigParser()
@@ -26,8 +49,13 @@ class kb_trimmomaticTest(unittest.TestCase):
         for nameval in config.items('kb_trimmomatic'):
             cls.cfg[nameval[0]] = nameval[1]
         cls.wsURL = cls.cfg['workspace-url']
+        cls.shockURL = cls.cfg['shock-url']
+        cls.handleURL = cls.cfg['handle-service-url']
+        cls.serviceWizardURL = cls.cfg['service-wizard-url']
+        
         cls.wsClient = workspaceService(cls.wsURL, token=token)
         cls.serviceImpl = kb_trimmomatic(cls.cfg)
+
 
     @classmethod
     def tearDownClass(cls):
@@ -53,28 +81,257 @@ class kb_trimmomaticTest(unittest.TestCase):
     def getContext(self):
         return self.__class__.ctx
 
-    def test_trimmomatic_ok(self):
-        print "run trim"
-        #ctx={'token':self.token}
-        input_params={}
-        input_params['input_ws']='psdehal:1455758523413'
-        input_params['input_read_library']='PE_reads_test'
-        input_params['read_type']='PE'
-        input_params['quality_encoding']='phred33'
-        input_params['adapterFa']='TruSeq3-PE.fa'
-        input_params['seed_mismatches']='2'
-        input_params['palindrome_clip_threshold']='30'
-        input_params['simple_clip_threshold']='10'
-        input_params['crop_length']='200'
-        input_params['head_crop_length']='1'
-        input_params['leading_min_quality']='1'
-        input_params['trailing_min_quality']='1'
-        input_params['sliding_window_size']='4'
-        input_params['sliding_window_min_quality']='5'
-        input_params['min_length']='80'
-        input_params['output_read_library']='testing'
-        input_params['output_ws'] = input_params['input_ws']
-        result=self.getImpl().runTrimmomatic(self.getContext(), input_params)
-        print result
 
-        
+    @classmethod
+    def upload_file_to_shock(cls, file_path):
+        """
+        Use HTTP multi-part POST to save a file to a SHOCK instance.
+        """
+
+        header = dict()
+        header["Authorization"] = "Oauth {0}".format(cls.token)
+
+        if file_path is None:
+            raise Exception("No file given for upload to SHOCK!")
+
+        with open(os.path.abspath(file_path), 'rb') as dataFile:
+            files = {'upload': dataFile}
+            response = requests.post(
+                cls.shockURL + '/node', headers=header, files=files,
+                stream=True, allow_redirects=True, timeout=30)
+
+        if not response.ok:
+            response.raise_for_status()
+
+        result = response.json()
+
+        if result['error']:
+            raise Exception(result['error'][0])
+        else:
+            return result["data"]
+
+
+    def getPairedEndLibInfo(self, read_lib_basename, lib_i=0):
+        if hasattr(self.__class__, 'pairedEndLibInfo_list'):
+            try:
+                info = self.__class__.pairedEndLibInfo_list[lib_i]
+                if info != None:
+                    return info
+            except:
+                pass
+
+        # 1) upload files to shock
+        token = self.ctx['token']
+        forward_shock_file = self.upload_file_to_shock('data/'+read_lib_basename+'.fwd.fq')
+        reverse_shock_file = self.upload_file_to_shock('data/'+read_lib_basename+'.rev.fq')
+        #pprint(forward_shock_file)
+        #pprint(reverse_shock_file)
+
+        # 2) create handle
+        hs = HandleService(url=self.handleURL, token=token)
+        forward_handle = hs.persist_handle({
+                                        'id' : forward_shock_file['id'], 
+                                        'type' : 'shock',
+                                        'url' : self.shockURL,
+                                        'file_name': forward_shock_file['file']['name'],
+                                        'remote_md5': forward_shock_file['file']['checksum']['md5']})
+
+        reverse_handle = hs.persist_handle({
+                                        'id' : reverse_shock_file['id'], 
+                                        'type' : 'shock',
+                                        'url' : self.shockURL,
+                                        'file_name': reverse_shock_file['file']['name'],
+                                        'remote_md5': reverse_shock_file['file']['checksum']['md5']})
+
+        # 3) save to WS
+        paired_end_library = {
+            'lib1': {
+                'file': {
+                    'hid':forward_handle,
+                    'file_name': forward_shock_file['file']['name'],
+                    'id': forward_shock_file['id'],
+                    'url': self.shockURL,
+                    'type':'shock',
+                    'remote_md5':forward_shock_file['file']['checksum']['md5']
+                },
+                'encoding':'UTF8',
+                'type':'fastq',
+                'size':forward_shock_file['file']['size']
+            },
+            'lib2': {
+                'file': {
+                    'hid':reverse_handle,
+                    'file_name': reverse_shock_file['file']['name'],
+                    'id': reverse_shock_file['id'],
+                    'url': self.shockURL,
+                    'type':'shock',
+                    'remote_md5':reverse_shock_file['file']['checksum']['md5']
+                },
+                'encoding':'UTF8',
+                'type':'fastq',
+                'size':reverse_shock_file['file']['size']
+
+            },
+            'interleaved':0,
+            'sequencing_tech':'artificial reads'
+        }
+
+        new_obj_info = self.wsClient.save_objects({
+                        'workspace':self.getWsName(),
+                        'objects':[
+                            {
+                                'type':'KBaseFile.PairedEndLibrary',
+                                'data':paired_end_library,
+                                'name':'test.pe.reads',
+                                'meta':{},
+                                'provenance':[
+                                    {
+                                        'service':'kb_trimmomatic',
+                                        'method':'test_runTrimmomatic'
+                                    }
+                                ]
+                            }]
+                        })[0]
+
+        # store it
+        if not hasattr(self.__class__, 'pairedEndLibInfo_list'):
+            self.__class__.pairedEndLibInfo_list = []
+        for i in range(lib_i):
+            try:
+                assigned = self.__class__.pairedEndLibInfo_list[i]
+            except:
+                self.__class__.pairedEndLibInfo_list.append(None)
+
+        self.__class__.pairedEndLibInfo_list.append(new_obj_info)
+        return new_obj_info
+
+
+    # call this method to get the WS object info of a Paired End Library Set (will
+    # upload the example data if this is the first time the method is called during tests)
+    def getPairedEndLib_SetInfo(self, read_libs_basename_list):
+        if hasattr(self.__class__, 'pairedEndLib_SetInfo'):
+            try:
+                info = self.__class__.pairedEndLib_SetInfo
+                if info != None:
+                    return info
+            except:
+                pass
+
+        # build items and save each PairedEndLib
+        items = []
+        for lib_i,read_lib_basename in enumerate (read_libs_basename_list):
+            label    = read_lib_basename
+            lib_info = self.getPairedEndLibInfo (read_lib_basename, lib_i)
+            lib_ref  = str(lib_info[6])+'/'+str(lib_info[0])
+            print ("LIB_REF["+str(lib_i)+"]: "+lib_ref+" read_lib_basename")  # DEBUG
+
+            items.append({'ref': lib_ref,
+                          'label': label
+                          #'data_attachment': ,
+                          #'info':
+                         })
+
+        # save readsset
+        desc = 'test ReadsSet'
+        readsSet_obj = { 'description': desc,
+                         'items': items
+                       }
+        name = 'TEST_READSET'
+
+        new_obj_info = self.wsClient.save_objects({
+                        'workspace':self.getWsName(),
+                        'objects':[
+                            {
+                                'type':'KBaseSets.ReadsSet',
+                                'data':readsSet_obj,
+                                'name':name,
+                                'meta':{},
+                                'provenance':[
+                                    {
+                                        'service':'kb_trimmomatic',
+                                        'method':'test_runTrimmomatic'
+                                    }
+                                ]
+                            }]
+                        })[0]
+
+        # store it
+        self.__class__.pairedEndLib_SetInfo = new_obj_info
+        return new_obj_info
+
+
+    ##############
+    # UNIT TESTS #
+    ##############
+
+
+    # NOTE: According to Python unittest naming rules test method names should start from 'test'.
+    #
+    def test_runTrimmomatic(self):
+
+        # Prepare test objects in workspace if needed using 
+        # self.getWsClient().save_objects({'workspace': self.getWsName(), 'objects': []})
+        #
+        # Run your method by
+        # ret = self.getImpl().your_method(self.getContext(), parameters...)
+        #
+        # Check returned data with
+        # self.assertEqual(ret[...], ...) or other unittest methods
+
+
+
+        ### TEST 1: run megahit against just one paired end library
+
+        # figure out where the test data lives
+        pe_lib_info = self.getPairedEndLibInfo('small_1')
+        pprint(pe_lib_info)
+
+        # Object Info Contents
+        # 0 - obj_id objid
+        # 1 - obj_name name
+        # 2 - type_string type
+        # 3 - timestamp save_date
+        # 4 - int version
+        # 5 - username saved_by
+        # 6 - ws_id wsid
+        # 7 - ws_name workspace
+        # 8 - string chsum
+        # 9 - int size
+        # 10 - usermeta meta
+
+        output_name = 'output_trim.PElib'
+        params = {
+            'input_ws': pe_lib_info[7],
+            'output_ws': pe_lib_info[7],
+            'input_reads_ref': str(pe_lib_info[6])+'/'+str(pe_lib_info[0]),
+            'output_read_name': output_name,
+            
+            'leading_min_quality': '1',
+            'trailing_min_quality': '1',
+            'crop_length': '200',
+            'head_crop_length': '1',
+            'min_length': '80',
+            'read_type': 'PE',
+            'quality_encoding': 'phred33',
+            'sliding_window': {
+                'sliding_window_size': '4',
+                'sliding_window_min_quality': '5'
+                },
+            'adapter_clip': {
+                'adapterFa': 'TruSeq3-PE.fa',
+                'seed_mismatches': '2',
+                'palindrome_clip_threshold': '30',
+                'simple_clip_threshold': '10'
+                }
+        }
+
+        result = self.getImpl().run_megahit(self.getContext(),params)
+        print('RESULT:')
+        pprint(result)
+
+        # check the output
+        info_list = self.wsClient.get_object_info([{'ref':pe_lib_info[7] + '/' + output_name}], 1)
+        self.assertEqual(len(info_list),1)
+        read_lib_info = info_list[0]
+        self.assertEqual(read_lib_info[1],output_name)
+        self.assertEqual(read_lib_info[2].split('-')[0],'KBaseFile.PairedEndLibrary')
